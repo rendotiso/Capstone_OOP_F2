3c3747f3cdeb001541bfc9fc3e1cc20e04cfa65a
Celfer Ronsh L. Paradela <celferronsh.paradela@cit.edu>
2025-11-25 20:59:09 +0800

    Update Food.java

--- src/Model/Entities/Food.java
+++ src/Model/Entities/Food.java
@@ -1,4 +1,8 @@
 package Model.Entities;
 
-public class Food {
+public class Food extends Item implements Expirable {
+  private String expiryDate;
+    private boolean isCanned;
+    private boolean isPerishable;
 }
+
