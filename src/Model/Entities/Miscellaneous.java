210836500c25f1945495d656c52b47ddda19ebee
Celfer Ronsh L. Paradela <celferronsh.paradela@cit.edu>
2025-11-25 19:42:21 +0800

    Update Miscellaneous.java

--- src/Model/Entities/Miscellaneous.java
+++ src/Model/Entities/Miscellaneous.java
@@ -1,4 +1,37 @@
 package Model.Entities;
 
-public class Miscellaneous {
+public class Miscellaneous extends Item {
+  
+    private String condition;
+    private String fabricType;
+    private char size;
+
+    public String getCondition(){
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getFabricType() {
+        return fabricType;
+    }
+
+    public void setFabricType(String fabricType) {
+        this.fabricType = fabricType;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
+
+    @Override
+    public double calculateValue() {
+        return purchasePrice * quantity;
+    }
 }
