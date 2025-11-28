5c57b5b06e423db061ba00d1b3d26d36bfa6e837
LAB12_USER ON WS25 <L12X09W25@LabHCI.net>
2025-11-28 08:40:41 +0800

    new commit

--- src/Model/Entities/Tools.java
+++ src/Model/Entities/Tools.java
@@ -1,4 +1,57 @@
 package Model.Entities;
 
-public class Tools {
+public class Tools extends Item implements Maintenanable{
+    private String toolType;
+    private boolean requiresMaintenance;
+    private String material;
+    private String lastMaintenanceDate;
+
+    public String getToolType() {
+        return toolType;
+    }
+
+    public void setToolType(String toolType) {
+        this.toolType = toolType;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public boolean getRequiresMaintenance() {
+        return requiresMaintenance;
+    }
+
+    public void setRequiresMaintenance(boolean requiresMaintenance) {
+        this.requiresMaintenance = requiresMaintenance;
+    }
+
+    @Override
+    public boolean needsMaintenance() {
+        return requiresMaintenance;
+    }
+
+    @Override
+    public void doMaintenance(){
+        return;
+    }
+
+    @Override
+    public String getLastMaintenanceDate() {
+        return lastMaintenanceDate;
+    }
+
+    @Override
+    public void setLastMaintenanceDate(String lastMaintenanceDate) {
+        this.lastMaintenanceDate = lastMaintenanceDate;
+    }
+
+    @Override
+    public double calculateValue(){
+        return 1.5;
+    }
 }
