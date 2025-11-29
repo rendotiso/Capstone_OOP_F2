0786e28a030afeec8555ef9fc909528d20276025
Andrea19_hub <clomanicole313@gmail.com>
2025-11-29 22:18:13 +0800

    new commit

--- src/Model/Entities/Item.java
+++ src/Model/Entities/Item.java
@@ -8,21 +8,18 @@ public abstract class Item {
     private String description;
     private int quantity;
     private double purchasePrice;
-    private String purchaseDate;
+    String purchaseDate;
     private String vendor;
     private Category category;
     private Location location;
 
-    public Item(String name,String description, int quantity, double purchasePrice, String purchaseDate, String vendor,Category category, Location location){
-        this.name = name;
-        this.description = description;
-        this.quantity = quantity;
-        this.purchasePrice = purchasePrice;
-        this.purchaseDate = purchaseDate;
-        this.vendor = vendor;
-        this.category = category;
-        this.location = location;
+    public Item(String name, String description, int quantity, double purchasePrice, String purchaseDate, String vendor, Category category, Location location) {
+
     }
+
+    public static boolean getCategory() {
+    }
+
     public String getName() {
         return name;
     }
@@ -39,19 +36,10 @@ public abstract class Item {
     public double getPurchasePrice() {
         return purchasePrice;
     }
-    
+
     public String getVendor() {
         return vendor;
     }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
 
     public void setName(String name) {
         this.name = name;
@@ -76,19 +64,10 @@ public abstract class Item {
     public void setVendor(String vendor) {
         this.vendor = vendor;
     }
-    
-    public void setCategory(Category category){
-        this.category = category;
-    }
-    
-    public void setLocation(Location location) {
-        this.location = location;
-    }
+
 
     public abstract void updateQuantity(int n);
+
     public abstract double calculateValue();
 
-    public String getPurchaseDate() {
-        return purchaseDate;
-    }
 }
