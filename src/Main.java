d95acd8a41ddb4c643a678872f36014208db2e37
HUAWEI <ravefcng@gmail.com>
2025-12-02 22:28:35 +0800

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 import UI.Dashboard;
+import UI.Panel.Food;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new Dashboard();
+        //new Dashboard();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createGUI();
+            }
+        });
+    }
+
+    private static void createGUI(){
+        Food food = new Food();
+        JPanel root = food.getRootPanel();
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(root);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
--- src/UI/Panel/Food.java
+++ src/UI/Panel/Food.java
@@ -1,4 +1,42 @@
 package UI.Panel;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 public class Food {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JComboBox comboBox1;
+    private JTextArea helloTextArea;
+    private JTable table1;
+    private JPanel rootPanel;
+    private JButton ADDButton;
+    private JButton CLEARButton;
+    private JButton UPDATEButton;
+    private JButton REMOVEButton;
+
+    public Food(){
+        createTable();
+    }
+
+    public JPanel getRootPanel(){
+        return rootPanel;
+    }
+
+    private void createTable(){
+        Object[][] data = {
+                {"Imong papa","Imong mama", "uwu", "uwu"},
+                {"Akoang papa","Akoang mama", "uwu", "uwu"},
+                {"Iyahang papa","Iyahang mama", "uwu", "uwu"}
+            };
+        table1.setModel(new DefaultTableModel(
+                data,
+                new String[]{"Title", "Year", "Rating", "Prices"}));
+    }
 }
