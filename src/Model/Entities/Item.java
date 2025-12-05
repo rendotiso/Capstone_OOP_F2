70de4a841fd55a9cef65c56ba437218002fdb319
renmaya <whatfu49@gmail.com>
2025-12-05 22:33:29 +0800

    commit Item Final2.0

--- src/Model/Entities/Item.java
+++ src/Model/Entities/Item.java
@@ -38,6 +38,8 @@ public abstract class Item {
     public String getVendor() {
         return vendor;
     }
+    public Category getCategory() { return category; }
+    public Location getLocation() { return location; }
 
     //SETTERS
     public void setName(String name) {
@@ -58,7 +60,8 @@ public abstract class Item {
     public void setVendor(String vendor) {
         this.vendor = vendor;
     }
-
+    public void setCategory(Category category) { this.category = category; }
+    public void setLocation(Location location) { this.location = location; }
 
     //ABSTRACT METHODS
     public String descriptionDetails() {
