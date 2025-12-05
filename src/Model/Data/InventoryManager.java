d7f9f5e41044edae73d30262fe36b5eb79fb96c2
Phygellus <ph4ntomgamer.ph@gmail.com>
2025-12-06 01:06:47 +0800

    InventoryManager methods

--- src/Model/Data/InventoryManager.java
+++ src/Model/Data/InventoryManager.java
@@ -11,15 +11,45 @@ public class InventoryManager {
     private List<Item> items;
     private final FileHandler fileHandler;
 
-  // ADDITEM
+    public InventoryManager(List<Item> items, FileHandler fileHandler) {
+        this.items = items;
+        this.fileHandler = fileHandler;
+    }
+
+    // ADDITEM
+    public void addItem(Item item){
+        items.add(item);
+        return;
+    }
 
   //DELETEITEM
+    public void removeItem(Item item){
+        items.remove(item);
+        return;
+    }
 
   //UPDATEITEM
+  public void updateItem(int index, Item updatedItem) {
+      if (index >= 0 && index < items.size()) {
+          items.set(index, updatedItem);
+          return;
+      }
+  }
 
   // GetAllItems, returns the List
+    public List<Item> GetAllItems() {
+        return items;
+    }
 
   // search and filter (get by category, get by location)
 
-  // save and load file
+    public List<Item> getbyCategory(Category category) {
+        return items.stream().filter(item -> item.getCategory() == category).collect(Collectors.toList());
+    }
+    public List<Item> getbyLocation(Location location) {
+        return items.stream().filter(item -> item.getLocation() == location).collect(Collectors.toList());
+    }
+
+
+    // save and load file
 }
